da801b501957043e2b32152b69127c23c39dcacc
Zéh Neto <zneto@live.com>
2018-11-06 12:26:12 -0300

    adicionados botões de nav

--- sistema.php
+++ sistema.php
@@ -5,7 +5,17 @@
   <?php require_once(NAV); ?>
 
    <section id="panel">      
-          <div class="col-lg-12 mx-auto text-center">
+          <div class="botoes-func col-lg-12 mx-auto text-center">
+
+          	<div class="col-lg-6 mx-auto text-center">
+          		<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+				<a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
+          	</div>
+          	<br>
+          	<div class="col-lg-6 mx-auto text-center">
+          		<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+				<a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
+          	</div>
           	
           </div>   
   </section>
