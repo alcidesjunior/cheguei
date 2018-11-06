7a03b917b5b3cf9ff28173cb98ed2615840117ea
Zéh Neto <zneto@live.com>
2018-11-06 12:25:49 -0300

    nomes alterados

--- assets/navbar.php
+++ assets/navbar.php
@@ -1,5 +1,5 @@
 <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark static-top">
+    <nav class="navbar navbar-expand-lg gradiente navbar-dark static-top">
       <div class="container">
         <a class="navbar-brand" href="sistema.php"><img class="img-fluid" src="assets/imgs/favicon.png" style="height: 28px; width: 28px;"/> Cheguei! Web App</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,13 +9,13 @@
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="cadastro.php">
-                <i class="fas fa-user-alt"></i> Cadastro de Funcionário
+                <i class="fas fa-user-alt"></i> Sua Conta
                 <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="frequencia.php">
-                <i class="fas fa-clipboard-list"></i> Controle de Frequência</a>
+                <i class="fas fa-clipboard-list"></i> Encerrar Sessão</a>
             </li>
           </ul>
         </div>
